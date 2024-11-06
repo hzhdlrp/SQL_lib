7c7395ed1de588155540c35364a91b0944bf8d42
hzhdlrp <pswjft@gmail.com>
2024-11-06 19:46:20 +0300

    savings

--- lib/memdb.cpp
+++ lib/memdb.cpp
@@ -1,4 +1,5 @@
 #include "memdb.hpp"
+#include "parser.hpp"
 
 memdb::ColumnInterface::ColumnInterface(std::string &s) {name = s;}
 memdb::ColumnInterface::ColumnInterface(std::string &s, ColumnAttributes &a) {name = s; attributes = a;}
@@ -14,3 +15,11 @@ memdb::Column<T>::Column(std::string &&s, T &&v) :  ColumnInterface(s) {value = 
 
 template<typename T>
 memdb::Column<T>::Column(std::string &&s, ColumnAttributes &&a, T &&v)  : ColumnInterface(s, a) {value = v;}
+
+template<typename... Types>
+void memdb::Table<Types...>::insert(Line<Types...> &&l) {lines.push_back(l);}
+
+template<typename... Types>
+std::type_info memdb::Table<Types...>::getType() const {
+    return typeid(Table<Types...>);
+}
--- lib/memdb.hpp
+++ lib/memdb.hpp
@@ -1,10 +1,8 @@
-#ifndef SQL_LIB_MEMBD_H
-#define SQL_LIB_MEMBD_H
-
-#endif //SQL_LIB_MEMBD_H
+#pragma once
 #include <string>
 #include <memory>
 #include <vector>
+#include <variant>
 
 namespace memdb {
 
@@ -17,33 +15,51 @@ namespace memdb {
     struct ColumnInterface {
         std::string name;
         ColumnAttributes attributes;
-
         ColumnInterface(std::string &);
         ColumnInterface(std::string &, ColumnAttributes &);
-
+    private:
         virtual void foo() = 0;
     };
 
     template <typename T>
     struct Column : public ColumnInterface {
         T value;
-
         Column(std::string &&);
-
         Column(std::string &&, T &&);
-
         Column(std::string &&, ColumnAttributes &&attr, T &&);
-
         Column(std::string &&, ColumnAttributes &&);
-
+    private:
+        void foo() override {};
     };
 
-    class Table {
-        std::vector<ColumnInterface> columns;
+    template<typename... Types>
+    class Line {
+        std::tuple<Types...> values;
+    };
+
+    class TableBase {
+        virtual std::type_info getType() const = 0;
+    };
+
+    template<typename... Types>
+    class Table : public TableBase{
+        std::vector<std::shared_ptr<ColumnInterface>> columns;
+        std::vector<std::type_info> types;
+        std::vector<Line<Types...>> lines;
+    public:
+        std::string name;
+        void insert(Line<Types...> &&);
+        std::type_info getType() const override;
     };
 
 
     class Database {
-        std::vector<Table> tables;
+        std::vector<std::shared_ptr<TableBase>> tables;
+        std::vector<std::type_info> types;
+
+    public:
+        void addNewTable(const std::shared_ptr<TableBase>&, const std::type_info);
+
     };
+
 }
