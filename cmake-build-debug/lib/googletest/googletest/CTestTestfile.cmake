ae7d330110215d68cfb17eb3fa2b9184a07c025b
hzhdlrp <pswjft@gmail.com>
2024-11-07 15:52:43 +0300

    savings

---
+++ cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vadimleonov/Desktop/плюсы/SQL_lib/lib/googletest/googletest
+# Build directory: /Users/vadimleonov/Desktop/плюсы/SQL_lib/cmake-build-debug/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
